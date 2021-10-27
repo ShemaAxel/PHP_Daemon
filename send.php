0520fa74bb821adc4ecc29fa238f0bab56ef74ad
Shema Romeo Axel <shemaromeoaxel@gmail.com>
2021-10-27 16:04:30 +0200

    Update send.php

--- send.php
+++ send.php
@@ -2,19 +2,17 @@
 require 'vendor/autoload.php';
 use AfricasTalking\SDK\AfricasTalking;
 
-$username = "axel";
-$apiKey = "412022e608798a4a37ca34d9bfe7f46a673da3be48c9d80ba3c8417d5ff9da48";
+$username = "";
+$apiKey = "";
 $AT = new AfricasTalking($username, $apiKey);
 $sms = $AT->sms();
-$recipients = "+250782980090";
-$penalty = 118;
-$totalAmount = 15986;
+$recipients = "";
+$penalty = ;
+$totalAmount = ;
 // Set your message
-$message = "Mukiriya wacu turabamenyeshako ukwezi ko kwishyura kwarenze.\n"
-    . "hiyongereyeho " . $penalty . "RWF yubukererwe,"
-    . "mukazishyura " . $totalAmount . "RWF.Murakoze"; //message
+$message = ""; //message
 // Set your shortCode or senderId
-$from = "churchService";
+$from = "";
 try {
     // Thats it, hit send and we'll take care of the rest
     $result = $sms->send([
